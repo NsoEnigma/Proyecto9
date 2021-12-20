f21068355ede7d3ee09b4795abcf1c92bee1e66c
Alejandro Asencio Montes <63419024+NsoEnigma@users.noreply.github.com>
2021-12-20 08:47:39 +0100

    nose

--- src/main/java/paquete/Proyecto9.java
+++ src/main/java/paquete/Proyecto9.java
@@ -6,13 +6,10 @@ public class Proyecto9 {
 	
 	public static void main(String[] args) {
 		Scanner lector = new Scanner(System.in);
-		float alturasSumadas=0;
+		short alturasSumadas=0;
 		byte nPersonas = 0;
-		String nombreLargo = "i";
+		String nombreLargo = "";
 		byte nHombres = 0;
-		byte nMujeres = 0;
-		char genero = 0;
-		int horasSumadas = 0;
 		int diasSumados = 0;
 		
 		do {
@@ -21,52 +18,54 @@ public class Proyecto9 {
 		} while (nPersonas < 2 || nPersonas > 10);
 		
 				
-		if (nPersonas >=2 || nPersonas <=10 ) {
 			
 			for(int i=0;i<nPersonas;i++) {
 				System.out.println("Dime el nombre de la persona");
 				String nombre = lector.nextLine();
-				if (nombre.length()>nombreLargo.length()){
-	                nombreLargo=nombre;
-	            }
-				
-				do {
-					System.out.println("Ahora dime su genero (h/m)");
-					genero = lector.nextLine().charAt(0);
-					if (genero == 'h') {
-						nHombres += 1;
-					}
-					
-					if (genero == 'm') {
-						nMujeres += 1;
-					}
-					
-				} while (genero != 'h' && genero != 'm');
 				
 				System.out.println("Ahora dime su altura en metros");
 				float altura = Float.parseFloat(lector.nextLine());
-				alturasSumadas+=altura;
 				
 				System.out.println("Dime su año de nacimiento");
 				short anioNacimiento = Short.parseShort(lector.nextLine());
-				System.out.println("Esta persona nacio en el siglo: " + ((anioNacimiento/100)+1));
-				int edad = (2021-anioNacimiento);
-				int dias = ((2021 - anioNacimiento) * 365);
-				int horas = (edad * 365)*24;
-				System.out.println("Tambien podemos decir que ha vivido un total de " +edad+ " años por lo cual a vivido " + dias + " dias y un total de " + horas + " horas." );
-				horasSumadas += horas;
-				diasSumados += dias;
+				
+				short siglo = (short) ((anioNacimiento/100)+1);
+				System.out.println("Has nacido en el siglo " + siglo);
+
+				System.out.println("Eres hombre (h) o mujer (m)?");
+				boolean hombre = 
+						lector.nextLine().toLowerCase().charAt(0) == 'h';
+				
+				if (nombreLargo.length()<nombre.length()){
+					nombreLargo=nombre;
+				}
+				
+				alturasSumadas+=altura*100;
+				
+				if(hombre) {nHombres++;}
+				
+				float nDias = (2021-anioNacimiento)*365.24f;
+				
+				System.out.println("Has vivido aproximadamente esta cantidad de dias");
+				System.out.println(nDias);
+				System.out.println("Has vivido aprox " + (nDias*24)+" horas");
+				diasSumados+=nDias;
+				
+				
+				
+				
+				
 				
 				
 
 			}
 			
-		}
-		System.out.println("La altura media seria de " + (alturasSumadas/nPersonas)*100 + " centímetros.");
+		
 		System.out.println("El nombre mas largo es " + nombreLargo);
-		System.out.println("Hay un total de " + nHombres + " hombres y de " + nMujeres + " mujeres.");
-		System.out.println("Los dias promedios vividos es de " + diasSumados + " dias.2");
-		System.out.println("Las horas promedias vividas es de " + horasSumadas + " horas.");
+		System.out.println("N hombres: " + nHombres);
+		System.out.println("N mujeres: " + (nPersonas-nHombres));
+		System.out.println("La altura media en cm es: " + (alturasSumadas/nPersonas));
+		System.out.println("El promedio de dias vividos es de " + (diasSumados/nPersonas));
 	}
 	
 }
